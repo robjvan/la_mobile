eb8911949a07073d5e7e030f1c6fe7dfafa597ab
Rob Vandelinder <robjvan@gmail.com>
2025-05-03 14:39:19 -0230

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:la_mobile/utilities/utilities.dart';
+import 'package:la_mobile/routes/app_routes.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+
+  /// Lock app in portrait orientation
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  /// Launch the app
+  runApp(MyApp());
+}
+
+@immutable
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(final BuildContext context) {
+    return GetMaterialApp(
+      title: 'app_title'.tr,
+      translations: AppTranslations(),
+      locale: Get.deviceLocale,
+      fallbackLocale: const Locale('en', 'US'),
+      getPages: AppRoutes.getPages,
+      theme: AppTheme.themeData,
+      initialRoute: AppRoutes.initialRoute,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
