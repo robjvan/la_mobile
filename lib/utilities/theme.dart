93860b2b4f6de41bb6a109c3f342d34a665b8494
Rob Vandelinder <robjvan@gmail.com>
2025-05-20 20:42:08 -0230

    Added color pickers

--- lib/utilities/theme.dart
+++ lib/utilities/theme.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:la_mobile/controllers/settings.controller.dart';
 
 @immutable
 class AppTheme {
+  static Locale fallbackLocale = const Locale('en', 'US');
+
   static ThemeData themeData = ThemeData(
-    primarySwatch: Colors.blue,
+    primarySwatch: Colors.green,
     visualDensity: VisualDensity.adaptivePlatformDensity,
+    primaryColorDark: Colors.lightGreen,
   );
+
+  static Color backgroundColor =
+      SettingsController.useDarkMode.value
+          ? AppColors.bgColorDarkMode
+          : AppColors.bgColorLightMode;
+
+  static Color accentColor =
+      SettingsController.useDarkMode.value
+          ? AppColors.green
+          : AppColors.limeGreen;
+
+  static Color textColor =
+      SettingsController.useDarkMode.value
+          ? AppColors.textColorDarkMode
+          : AppColors.textColorLightMode;
+
+  static Color secondaryTextColor =
+      SettingsController.useDarkMode.value
+          ? AppColors.green
+          : AppColors.limeGreen;
 }
 
 @immutable
-class AppStyles {}
+class AppStyles {
+  // TODO(RV): Add styles for text, buttons, etc.
+}
 
 @immutable
 class AppColors {
@@ -20,12 +46,14 @@ class AppColors {
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-  static const Color green = Color(0xFF00FF00);
+  static const Color limeGreen = Color(0xFF00FF00);
+  static const Color green = Color(0xFF005500);
+  static const Color darkGreen = Color(0xFF002200);
+  static const Color lightBlue = Color(0xFF2196F3);
   static const Color blue = Color(0xFF0000FF);
   static const Color red = Color(0xFFFF0000);
   static const Color grey = Color(0xFF808080);
   static const Color lightGrey = Color(0xFFDDDDDD);
-  static const Color locationTileDarkGrey = Color(0xFF303030);
-  static const Color darkGrey = Color(0xFF202020);
+  static const Color darkGrey = Color(0xFF181818);
   static const Color lavender = Color(0xFFD0BCFF);
 }
