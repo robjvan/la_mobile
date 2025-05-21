ee93d6e523b83907d4e58cc8a7196192e8ed0939
Rob Vandelinder <robjvan@gmail.com>
2025-05-21 00:54:25 -0230

    Added searchBar and filterBar methods

--- lib/controllers/settings.controller.dart
+++ lib/controllers/settings.controller.dart
@@ -1,7 +1,13 @@
 import 'package:get/get_rx/get_rx.dart';
 
 class SettingsController {
+  static RxBool isLoading = false.obs;
+
   static RxBool useDarkMode = false.obs;
+
+  static RxBool showFilterBar = false.obs;
+
+  static RxBool viewAsList = false.obs;
 
   static void toggleDarkMode() {
     if (SettingsController.useDarkMode.value) {
@@ -9,5 +15,23 @@ class SettingsController {
     } else {
       SettingsController.useDarkMode.value = true;
     }
+  }
+
+  static void toggleFilterBar() {
+    if (SettingsController.showFilterBar.value) {
+      SettingsController.showFilterBar.value = false;
+    } else {
+      SettingsController.showFilterBar.value = true;
+    }
+    print(SettingsController.showFilterBar.value);
+  }
+
+  static void toggleListView() {
+    if (SettingsController.viewAsList.value) {
+      SettingsController.viewAsList.value = false;
+    } else {
+      SettingsController.viewAsList.value = true;
+    }
+    print(SettingsController.viewAsList.value);
   }
 }
