fad755a1715e6af1db94a30632626af428a71521
Rob Vandelinder <robjvan@gmail.com>
2025-05-21 00:51:16 -0230

    Added data fields

--- lib/models/user.model.dart
+++ lib/models/user.model.dart
@@ -1,17 +1,39 @@
 class UserModel {
   final String? username;
   final int? userId;
-  // TODO: Add missing fields
+  final int? roleId;
+  final String? lastLogin;
+  final String? accessToken;
 
-  UserModel({this.username, this.userId});
+  UserModel({
+    required this.username,
+    required this.userId,
+    required this.roleId,
+    required this.lastLogin,
+    required this.accessToken,
+  });
 
-  factory UserModel.initial() => UserModel();
+  factory UserModel.initial() => UserModel(
+    username: null,
+    userId: null,
+    roleId: null,
+    lastLogin: null,
+    accessToken: null,
+  );
 
-  factory UserModel.fromMap(final Map<String, dynamic> json) =>
-      UserModel(username: json['username'], userId: json['userId']);
+  factory UserModel.fromMap(final Map<String, dynamic> json) => UserModel(
+    username: json['username'],
+    userId: json['userId'],
+    roleId: json['roleId'],
+    lastLogin: json['lastLogin'],
+    accessToken: json['accessToken'],
+  );
 
   Map<String, dynamic> toMap() => <String, dynamic>{
     'username': username,
     'userId': userId,
+    'roleId': roleId,
+    'lastLogin': lastLogin,
+    'accessToken': accessToken,
   };
 }
