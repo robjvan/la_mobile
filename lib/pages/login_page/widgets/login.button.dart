959a3e3934d7aea2f03ce8f8bed2e39485bb5121
Rob Vandelinder <robjvan@gmail.com>
2025-05-21 00:52:55 -0230

    Added call to fetch user plants

--- lib/pages/login_page/widgets/login.button.dart
+++ lib/pages/login_page/widgets/login.button.dart
@@ -4,11 +4,12 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:la_mobile/constants.dart';
+import 'package:la_mobile/controllers/plants.controller.dart';
 import 'package:la_mobile/controllers/user.controller.dart';
 import 'package:la_mobile/models/user.model.dart';
 import 'package:la_mobile/services/network.service.dart';
 import 'package:la_mobile/widgets/dialogs/bad_credentials.dialog.dart';
-import 'package:la_mobile/widgets/la_button.dart';
+import 'package:la_mobile/widgets/buttons/la_button.dart';
 
 class LoginButton extends StatelessWidget {
   final GlobalKey<FormState> formKey;
@@ -35,6 +36,9 @@ class LoginButton extends StatelessWidget {
             UserController.setUserData(
               UserModel.fromMap(jsonDecode(response.body)),
             );
+
+            // Fetch user data - user, profile, plants, etc.
+            await PlantsController.fetchUserPlants();
 
             unawaited(Get.offAllNamed(kHomeRouteName));
           } else {
