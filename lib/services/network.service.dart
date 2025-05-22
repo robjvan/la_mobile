41bd6c6b42c8de156f80f3c16154646ef2e1c765
Rob Vandelinder <robjvan@gmail.com>
2025-05-22 20:23:56 -0230

    Moved API calls from controllers to services

--- lib/services/network.service.dart
+++ lib/services/network.service.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
-// import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:la_mobile/constants.dart';
-import 'package:la_mobile/controllers/user.controller.dart';
 import 'package:la_mobile/secrets.dart';
 
-// This class is responsible for making API calls and handling responses.
 class NetworkService {
   static Future<http.Response> checkNetworkConnection() async {
     try {
@@ -18,45 +15,17 @@ class NetworkService {
       if (result.statusCode == 200 && result.body == 'pong') {
         return result;
       } else {
-        return http.Response('Could not connect to server', 404);
+        return http.Response(
+          'Could not connect to server'.tr, // TODO(RV): Add i18n strings
+          404,
+        );
       }
     } on Exception catch (err) {
       debugPrint(err.toString());
-      return http.Response('Could not connect to server', 404);
-    }
-  }
-
-  static Future<dynamic> login(
-    final String username,
-    final String password,
-  ) async {
-    try {
-      final http.Response response = await http.post(
-        Uri.parse('${AppSecrets.serverUrl}/$kLoginEndpoint'),
-        body: <String, String>{'username': username, 'password': password},
+      return http.Response(
+        'Could not connect to server', // TODO(RV): Add i18n strings
+        404,
       );
-
-      return response;
-    } on Exception catch (err) {
-      print(err);
-    }
-  }
-
-  static Future<dynamic> fetchUserPlants() async {
-    try {
-      final http.Response response = await http.get(
-        Uri.parse(
-          '${AppSecrets.serverUrl}/$kPlantsEndpoint/byuser/${UserController.user.value.userId}',
-        ),
-        headers: <String, String>{
-          'Authorization': 'Bearer ${UserController.user.value.accessToken}',
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-      );
-
-      return response;
-    } on Exception catch (err) {
-      print(err);
     }
   }
 }
---
+++ lib/services/plants.service.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:la_mobile/constants.dart';
+import 'package:la_mobile/controllers/app_state.controller.dart';
+import 'package:la_mobile/controllers/user_state.controller.dart';
+import 'package:la_mobile/models/plant.model.dart';
+import 'package:la_mobile/secrets.dart';
+
+class PlantsService {
+  static Map<String, String> buildAuthHeaders() {
+    return <String, String>{
+      'Authorization': 'Bearer ${UserStateController.user.value.accessToken}',
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+  }
+
+  static Future<void> fetchUserPlants() async {
+    try {
+      AppStateController.isLoading.value = true;
+
+      final http.Response response = await http.get(
+        Uri.parse(
+          '${AppSecrets.serverUrl}/$kPlantsEndpoint/byuser/${UserStateController.user.value.userId}',
+        ),
+        headers: buildAuthHeaders(),
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> plantList = jsonDecode(response.body);
+
+        UserStateController.userPlants.value =
+            plantList
+                .map((final dynamic plant) => PlantModel.fromMap(plant))
+                .toList();
+      }
+
+      AppStateController.isLoading.value = false;
+    } on Exception catch (err) {
+      print(err);
+    }
+  }
+
+  static Future<PlantModel?> addNewPlant(final PlantModel plant) async {
+    // final dynamic response = await NetworkService.addNewPlant(plant);
+    try {
+      AppStateController.isLoading.value = true;
+
+      final http.Response response = await http.post(
+        Uri.parse('${AppSecrets.serverUrl}/$kPlantsEndpoint/'),
+        body: plant,
+        headers: buildAuthHeaders(),
+      );
+
+      // Reset the UI
+      AppStateController.isLoading.value = false;
+
+      // Check to ensure record was returned successfully
+      if (response.statusCode == 201 &&
+          jsonDecode(response.body)['createdAt'] != null) {
+        // Update our local plants list in the controller
+        await PlantsService.fetchUserPlants();
+
+        return PlantModel.fromMap(jsonDecode(response.body));
+      }
+      return null;
+    } on Exception catch (err) {
+      print(err);
+      return null;
+    }
+  }
+
+  static Future<void> markPlantAsWatered() async {
+    // TODO(RV): Add logic
+  }
+
+  static Future<void> markPlantAsFertilized() async {
+    // TODO(RV): Add logic
+  }
+
+  static void clearPlants() {
+    UserStateController.userPlants.value = <PlantModel>[];
+  }
+}
