2237e23e0d901c498a66d5adf788bff3106e89c7
Rob Vandelinder <robjvan@gmail.com>
2025-05-27 19:52:36 -0230

    Fixed typo in filename

--- lib/pages/home_page/widgets/plant_list_tile.dart
+++ lib/pages/home_page/widgets/plant_list_tile.dart
@@ -10,6 +10,17 @@ class PlantListTile extends StatelessWidget {
   final PlantModel plant;
   const PlantListTile(this.plant, {super.key});
 
+  Icon? buildTrailingIcon() {
+    Icon? icon;
+
+    if ((plant.imageUrls == null) || (plant.imageUrls!.isEmpty)) {
+      icon = Icon(Icons.warning_rounded, color: AppColors.red);
+    }
+    // TODO(RV): Add check for overdue watering or fertilizing, overwrite icon as necessary
+
+    return icon;
+  }
+
   @override
   Widget build(final BuildContext context) {
     return ListTile(
@@ -17,7 +28,9 @@ class PlantListTile extends StatelessWidget {
         Get.dialog(PlantDetailsDialog(plant));
       },
       leading:
-          (plant.imageUrls != null) && (plant.imageUrls!.isNotEmpty)
+          (plant.imageUrls != null) &&
+                  plant.imageUrls!.isNotEmpty &&
+                  plant.imageUrls![0] != null
               ? CachedNetworkImage(imageUrl: plant.imageUrls![0])
               : Image.asset('assets/images/image_placeholder.png'),
       title: Text(
@@ -31,14 +44,11 @@ class PlantListTile extends StatelessWidget {
       ),
       subtitle: Text(
         plant.lastWateredAt != null
-            ? 'Last watered: ${plant.lastWateredAt}'
+            ? 'Last watered: ${plant.lastWateredAt!.substring(0, 10)}'
                 .tr // TODO(RV): Add i18n strings
-            : 'No watering records yet!'.tr, // TODO(RV): Add i18n strings
+            : 'plants.no-records'.tr,
       ),
-      trailing:
-          (plant.imageUrls == null) || (plant.imageUrls!.isEmpty)
-              ? Icon(Icons.warning_rounded, color: AppColors.red)
-              : null,
+      trailing: buildTrailingIcon(),
     );
   }
 }
