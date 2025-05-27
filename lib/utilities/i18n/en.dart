ea721a2d146d768102755d8a9d950ca6db150376
Rob Vandelinder <robjvan@gmail.com>
2025-05-27 19:49:57 -0230

    Added translation files

--- lib/utilities/i18n/en.dart
+++ lib/utilities/i18n/en.dart
@@ -1,5 +1,6 @@
 /// English strings used for translations.
 final Map<String, String> englishStrings = <String, String>{
+  // Common strings
   'app_title': 'LeafAlert',
   'cancel': 'Cancel',
   'submit': 'Submit',
@@ -9,18 +10,31 @@ final Map<String, String> englishStrings = <String, String>{
   'loading': 'Loading...',
   'yes': 'Yes',
   'no': 'No',
-  //
-  'drawer.about_app': 'About LeafAlert',
-  //
-  'about.legalese':
-      'Copyright © 2025 by Rob Vandelinder.  All rights reserved.',
+  'choose-date': 'Choose date',
+  'tags': 'Tags',
+  'close': 'Close',
+
+  // Speed dial string
+  'speed-dial.add-new-plant': 'Add new plant',
+  'speed-dial.record-watering': 'Record watering',
+  'speed-dial.record-fertilizing': 'Record fertilizing',
+  'speed-dial.view-insights': 'View Insights',
+  'speed-dial.admin-dsahboard': 'Admin Dashboard',
+
+  // About app strings
+  'about_app': 'About LeafAlert',
+  'legalese': 'Copyright © 2025 by Rob Vandelinder.  All rights reserved.',
+
   //
   'collection': 'Collection',
+
   //
   'dashboard': 'Dashboard',
+
   //
   'favorites': 'Favorites',
-  //
+
+  // Login page strings
   'login': 'Login',
   'login.email-error': 'Email not confirmed',
   'login.email-error-msg': 'Please confirm email address and try again.',
@@ -31,15 +45,20 @@ final Map<String, String> englishStrings = <String, String>{
   'login.logging-in': 'Logging in...',
   'login.password': 'Password',
   'login.password-error': 'Password cannot be empty',
-  'login.register': 'Register',
+  'login.password-too-short': 'Password must at least 6 digits',
+  'login.register': 'New user? Register here',
   'login.reset-password': 'Reset Password',
   'login.username': 'Username',
   'login.username-error': 'Username cannot be empty',
-  //
+  'login.remember-username': 'Rembember username?',
+  'login.invalid-username': 'Username must be a valid email address',
+
+  // Network error strings
   'network.error': 'Network Error',
   'network.error-msg':
       'Could not connect to the platform server, please try again later.',
-  //
+
+  // Profile strings
   'profile': 'Profile',
   'profile.billing_address': 'Billing Address',
   'profile.collection_count': 'You have @numPlants @msg in your collection',
@@ -64,12 +83,91 @@ final Map<String, String> englishStrings = <String, String>{
   'profile.receive_newsletter': 'Receive Newsletter',
   'profile.update_address': 'Update Address',
   'profile.update-password': 'Update Password',
-  //
+
+  // Settings strings
   'settings': 'Settings',
   'settings.global_count': '@numPlants plants in the global database',
-  //
+
+  // Splash strings
   'splash.connected': 'Connected!',
   'splash.connecting': 'Connecting to server ...',
   'splash.connection-failed':
       'Could not connect to the LeafAlert server, please try again later.',
+
+  // Metrics strings
+  'user-metrics': 'Users',
+  'total-users': 'Total Users',
+  'active-users': 'Active Users',
+  'power-users': 'Power Users',
+  'have-reviewed': 'Have Reviewed',
+  'email-not-confirmed': 'Email Not Confirmed',
+  'user-login-metrics': 'User Logins',
+  'totalLogins': 'Total Logins',
+  'totalLoginsToday': 'Total Logins Today',
+  'totalLoginsThisWeek': 'Total Logins This Week',
+  'totalLoginsThisMonth': 'Total Logins This Month',
+  'totalLogins7days': 'Total Logins Past 7 Days',
+  'totalLogins30days': 'Total Logins Past 30 Days',
+  'totalLogins90days': 'Total Logins Past 90 Days',
+  'totalLoginsThisYear': 'Total Logins This Year',
+  'user-growth-metrics': 'User Growth',
+  'userGrowthRate7days': 'User Growth Past 7 Days',
+  'userGrowthRate30days': 'User Growth Past 30 Days',
+  'userGrowthRate90days': 'User Growth Past 90 Days',
+  'userGrowthRateAnnual': 'User Growth Past Year',
+  'plant-metrics': 'Plants',
+  'live-plants': 'Total Live Plants',
+  'toBeWateredToday': 'Plants To Be Watered Today',
+  'mostWateredSpecies': 'Most Watered Species',
+  'wateringFrequency-min': 'Min Watering Frequency',
+  'wateringFrequency-avg': 'Avg Watering Frequency',
+  'wateringFrequency-max': 'Max Watering Frequency',
+  'fertilizingFrequency-min': 'Min Fertilizing Frequency',
+  'fertilizingFrequency-avg': 'Avg Fertilizing Frequency',
+  'fertilizingFrequency-max': 'Max Fertilizing Frequency',
+  'mostFertilizedSpecies': 'Most Fertilized Species',
+  'plantsAddedToday': 'Plants Added Today',
+  'plantsAdded7Days': 'Plants Added Past 7 Days',
+  'plantsAdded30Days': 'Plants Added Past 30 Days',
+  'plantsAdded90Days': 'Plants Added Past 90 Days',
+  'new-user-metrics': 'New Users',
+  'newUsersThisWeek': 'New Users This Week',
+  'newUsersLastWeek': 'New Users Last Week',
+  'newUsersThisMonth': 'New Users This Month',
+  'newUsersLastMonth': 'New Users Last Month',
+  'newUsers7days': 'New Users Past 7 Days',
+  'newUsers30days': 'New Users Past 30 Days',
+  'newUsers90days': 'New Users Past 90 Days',
+  'geographical-metrics': 'Geographical',
+  'usersInCanada': 'Users In Canada',
+  'usersInUSA': 'Users In USA',
+  'usersInOther': 'Users in Other',
+  'topCountryByUsers': 'Top Country By Users',
+  'topCountryNumUsers': 'Top Country Num Users',
+  'topCountryByLogins': 'Top Country By Logins',
+  'topCountryNumLogins': 'Top Country Num Logins',
+
+  // New plant page string
+  'add-plant': 'Add Plant',
+  'new-plant.no-empty-watered-date':
+      'Last watered date cannot be empty if watering reminders are enabled',
+  'new-plant.no-empty-fertiized-date':
+      'Last fertilized date cannot be empty if fertilizing reminders are enabled',
+  'new-plant.name-hint': 'e.g. "Spider Plant" or "Mom\'s Lily"',
+  'new-plant.no-empty-name': 'Name cannot be empty!',
+  'new-plant.species-hint': 'e.g. "Chlorophytum comosum" or "African Lily"',
+  'new-plant.location-hint': 'e.g. "Kitchen" or "Bedroom"',
+  'new-plant.watering-reminders': 'Enable watering reminders?',
+  'new-plant.watering-interval': 'Watering interval (days):',
+  'new-plant.last-watered': 'Last watered (optional):',
+  'new-plant.fertilizer-reminders': 'Enable fertilizer reminders?',
+  'new-plant.fertilizer-interval': 'Fertilizer interval (days):',
+  'new-plant.last-fertilized': 'Last fertilized (optional):',
+  'new-plant.upload-photo': 'Upload a photo',
+  'new-plant.notes': 'Notes',
+  'new-plant.notes-hint': 'e.g. "Avoid direct sunlight"',
+  'new-plant.tags-hint': 'e.g. "cactus" or "flowers"',
+
+  // Plant strings
+  'plants.no-records': 'No watering records yet!',
 };
