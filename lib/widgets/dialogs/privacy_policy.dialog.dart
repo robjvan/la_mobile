689da797930da257e220e1806a759b3d4f9b6bcd
Rob Vandelinder <robjvan@gmail.com>
2025-05-27 19:55:49 -0230

    Swapped page for dialog

---
+++ lib/widgets/dialogs/privacy_policy.dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrivacyPolicyDialog extends StatelessWidget {
+  const PrivacyPolicyDialog({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return const SimpleDialog();
+  }
+}
