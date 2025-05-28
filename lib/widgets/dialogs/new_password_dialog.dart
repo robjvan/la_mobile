511fb694e281d2efb98dced279fe99d6517a90f0
Rob Vandelinder <robjvan@gmail.com>
2025-05-28 10:39:23 -0230

    Added dialog placeholder files

---
+++ lib/widgets/dialogs/new_password_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewPasswordDialog extends StatelessWidget {
+  const NewPasswordDialog({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    // "Copy token from message to this screen to reset password"
+    // When new password is submitted, token is verified.  If token is OK, password is updated.
+    // If OK response is returned, update UI message to confirm password has been changed
+    // When user clicks "Ok", route to login page
+    // TODO(RV): Add logic
+    return const Placeholder();
+  }
+}
