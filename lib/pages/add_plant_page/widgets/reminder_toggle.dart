1ab4baacb9b742ba2ec25885c7fd26151dfd20aa
Rob Vandelinder <robjvan@gmail.com>
2025-06-03 22:15:40 -0230

    Added text styling

--- lib/pages/add_plant_page/widgets/reminder_toggle.dart
+++ lib/pages/add_plant_page/widgets/reminder_toggle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:la_mobile/utilities/theme.dart';
 
 class LaReminderToggle extends StatelessWidget {
   final String label;
@@ -16,7 +17,11 @@ class LaReminderToggle extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Text(label, style: TextStyle()),
+        Text(
+          label,
+          style: TextStyle(color: AppTheme.textColor(), fontSize: 18.0),
+          overflow: TextOverflow.ellipsis,
+        ),
         Checkbox(value: condition, onChanged: onChanged),
       ],
     );
