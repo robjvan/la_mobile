238e569ab4fe093020802e19b437b0faae7b0e86
Rob Vandelinder <robjvan@gmail.com>
2025-07-14 21:23:11 -0230

    Added icons for reminders

--- lib/pages/home_page/widgets/plant_grid_tile.dart
+++ lib/pages/home_page/widgets/plant_grid_tile.dart
@@ -11,6 +11,78 @@ class PlantGridTile extends StatelessWidget {
   final PlantModel plant;
   const PlantGridTile(this.plant, {super.key});
 
+  List<Widget> buildTrailingIcons() {
+    final List<Widget> icons = <Widget>[];
+
+    // Overdue watering logic
+    if (plant.lastWateredAt != null && plant.waterIntervalDays != null) {
+      try {
+        final DateTime lastWatered = DateTime.parse(plant.lastWateredAt!);
+        final DateTime nextWatering = lastWatered.add(
+          Duration(days: plant.waterIntervalDays!),
+        );
+
+        if (DateTime.now().isAfter(nextWatering)) {
+          icons.add(
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+              padding: const EdgeInsets.all(4.0),
+              child: Icon(
+                Icons.opacity_rounded,
+                color: AppColors.lightBlue,
+                size: 16.0,
+              ),
+            ),
+          ); // water droplet icon
+        }
+      } on Exception catch (_) {
+        icons.add(Icon(Icons.error_outline, color: AppColors.red, size: 16.0));
+      }
+    }
+
+    // Overdue fertilizing logic
+    if (plant.lastFertilizedAt != null &&
+        plant.fertilizerIntervalDays != null) {
+      try {
+        final DateTime lastFertilized = DateTime.parse(plant.lastFertilizedAt!);
+        final DateTime nextFertilizing = lastFertilized.add(
+          Duration(days: plant.fertilizerIntervalDays!),
+        );
+
+        if (DateTime.now().isAfter(nextFertilizing)) {
+          icons.add(
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+              padding: const EdgeInsets.all(4.0),
+              child: Icon(Icons.grain, color: Colors.brown, size: 16.0),
+            ),
+          ); // fertilizer icon
+        }
+      } on Exception catch (_) {
+        icons.add(Icon(Icons.error, color: AppColors.red, size: 16.0));
+      }
+    }
+
+    // Interval set but reminder not enabled interval
+    // if (plant.waterIntervalDays != null &&
+    //     plant.wateringReminderEnabled == false) {
+    //   icons.add(Icon(Icons.timer_off, color: AppColors.grey, size: 16.0));
+    // }
+
+    // if (plant.fertilizerReminderEnabled &&
+    //     plant.fertilizerIntervalDays == null) {
+    //   icons.add(Icon(Icons.timer_off, color: AppColors.grey, size: 16.0));
+    // }
+
+    return icons;
+  }
+
   @override
   Widget build(final BuildContext context) {
     return GestureDetector(
@@ -19,42 +91,54 @@ class PlantGridTile extends StatelessWidget {
       },
       child: ClipRRect(
         borderRadius: BorderRadiusGeometry.circular(8),
-        child: GridTile(
-          footer: DecoratedBox(
-            decoration: BoxDecoration(color: Colors.black54),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 4.0, 0, 4.0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    plant.name ?? '',
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(color: AppColors.white),
-                  ),
-                  Spacer(),
-                  (plant.lastWateredAt == null)
-                      ? Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          Icons.warning_rounded,
-                          size: 16.0,
-                          color: AppColors.red,
+        child: Stack(
+          children: <Widget>[
+            GridTile(
+              // header: Container(
+              //   padding: const EdgeInsets.only(top: 4.0, right: 4.0, bottom: 4.0),
+              //   color: Colors.red.withAlpha(220),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.end,
+              //     children: buildTrailingIcons(),
+              //   ),
+              // ),
+              footer: DecoratedBox(
+                decoration: BoxDecoration(color: Colors.black54),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        // width: 70,
+                        child: Text(
+                          plant.name ?? '',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(color: AppColors.white),
                         ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              child:
+                  (plant.imageUrls != null) &&
+                          plant.imageUrls!.isNotEmpty &&
+                          plant.imageUrls![0] != null
+                      ? CachedNetworkImage(
+                        imageUrl: plant.imageUrls![0],
+                        fit: BoxFit.cover,
                       )
-                      : Container(),
-                ],
+                      : Image.asset('assets/images/image_placeholder.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(4.0),
+              child: Row(
+                spacing: 4.0,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: buildTrailingIcons(),
               ),
             ),
-          ),
-          child:
-              (plant.imageUrls != null) &&
-                      plant.imageUrls!.isNotEmpty &&
-                      plant.imageUrls![0] != null
-                  ? CachedNetworkImage(
-                    imageUrl: plant.imageUrls![0],
-                    fit: BoxFit.cover,
-                  )
-                  : Image.asset('assets/images/image_placeholder.png'),
+          ],
         ),
       ),
     );
--- lib/pages/home_page/widgets/plant_list_tile.dart
+++ lib/pages/home_page/widgets/plant_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:la_mobile/controllers/app_state.controller.dart';
 import 'package:la_mobile/models/plant.model.dart';
 import 'package:la_mobile/utilities/theme.dart';
 import 'package:la_mobile/widgets/dialogs/plant_details.dialog.dart';
@@ -9,47 +10,101 @@ class PlantListTile extends StatelessWidget {
   final PlantModel plant;
   const PlantListTile(this.plant, {super.key});
 
-  Icon? buildTrailingIcon() {
-    Icon? icon;
+  List<Icon> buildTrailingIcons() {
+    final List<Icon> icons = <Icon>[];
 
-    if (plant.lastWateredAt == null) {
-      icon = Icon(Icons.warning_rounded, color: AppColors.red);
+    // Overdue watering logic
+    if (plant.lastWateredAt != null && plant.waterIntervalDays != null) {
+      try {
+        final DateTime lastWatered = DateTime.parse(plant.lastWateredAt!);
+        final DateTime nextWatering = lastWatered.add(
+          Duration(days: plant.waterIntervalDays!),
+        );
+
+        if (DateTime.now().isAfter(nextWatering)) {
+          icons.add(
+            Icon(Icons.opacity_rounded, color: AppColors.lightBlue),
+          ); // water droplet icon
+        }
+      } on Exception catch (_) {
+        icons.add(Icon(Icons.error_outline, color: AppColors.red));
+      }
     }
-    // TODO(RV): Add check for overdue watering or fertilizing, overwrite icon as necessary
 
-    return icon;
+    // Overdue fertilizing logic
+    if (plant.lastFertilizedAt != null &&
+        plant.fertilizerIntervalDays != null) {
+      try {
+        final DateTime lastFertilized = DateTime.parse(plant.lastFertilizedAt!);
+        final DateTime nextFertilizing = lastFertilized.add(
+          Duration(days: plant.fertilizerIntervalDays!),
+        );
+
+        if (DateTime.now().isAfter(nextFertilizing)) {
+          icons.add(Icon(Icons.grain, color: Colors.brown)); // fertilizer icon
+        }
+      } on Exception catch (_) {
+        icons.add(Icon(Icons.error, color: AppColors.red));
+      }
+    }
+
+    // Interval set but reminder not enabled interval
+    // if (plant.waterIntervalDays != null &&
+    //     plant.wateringReminderEnabled == false) {
+    //   icons.add(Icon(Icons.timer_off, color: AppColors.grey));
+    // }
+
+    // if (plant.fertilizerReminderEnabled &&
+    //     plant.fertilizerIntervalDays == null) {
+    //   icons.add(Icon(Icons.timer_off, color: AppColors.grey));
+    // }
+
+    return icons;
   }
 
   @override
   Widget build(final BuildContext context) {
-    return ListTile(
-      onTap: () {
-        Get.dialog(PlantDetailsDialog(plant));
-      },
-      leading: ClipRRect(
-        borderRadius: BorderRadiusGeometry.circular(4),
-        child:
-            (plant.imageUrls != null) &&
-                    plant.imageUrls!.isNotEmpty &&
-                    plant.imageUrls![0] != null
-                ? CachedNetworkImage(
-                  imageUrl: plant.imageUrls![0],
-                  width: 56.0,
-                  fit: BoxFit.cover,
-                )
-                : Image.asset('assets/images/image_placeholder.png'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+      child: Obx(
+        () => ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(10),
+          ),
+          tileColor:
+              AppStateController.useDarkMode.value
+                  ? Colors.white10
+                  : const Color(0xFFEEEEEE),
+          onTap: () => Get.dialog(PlantDetailsDialog(plant)),
+          leading: ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(4),
+            child:
+                (plant.imageUrls != null) &&
+                        plant.imageUrls!.isNotEmpty &&
+                        plant.imageUrls![0] != null
+                    ? CachedNetworkImage(
+                      imageUrl: plant.imageUrls![0],
+                      width: 56.0,
+                      fit: BoxFit.cover,
+                    )
+                    : Image.asset('assets/images/image_placeholder.png'),
+          ),
+          title: Text(
+            plant.name ?? '',
+            style: TextStyle(color: AppTheme.textColor()),
+          ),
+          subtitle: Text(
+            plant.lastWateredAt != null
+                ? 'Last watered: ${plant.lastWateredAt!.substring(0, 10)}'
+                    .tr // TODO(RV): Add i18n strings
+                : 'plants.no-records'.tr,
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: buildTrailingIcons(),
+          ),
+        ),
       ),
-      title: Text(
-        plant.name ?? '',
-        style: TextStyle(color: AppTheme.textColor()),
-      ),
-      subtitle: Text(
-        plant.lastWateredAt != null
-            ? 'Last watered: ${plant.lastWateredAt!.substring(0, 10)}'
-                .tr // TODO(RV): Add i18n strings
-            : 'plants.no-records'.tr,
-      ),
-      trailing: buildTrailingIcon(),
     );
   }
 }
